db712f6043ada516862c76741ce141475d817114
Grace Mekarski <gracemekarski@Grace-Mekarski-2.local>
2015-06-17 15:04:15 -0700

    change erb file to html file and make minor content revisions

--- provision/setup.sh
+++ provision/setup.sh
@@ -8,12 +8,6 @@ apt-get install git -y > /dev/null
 echo "Installing Nginx"
 apt-get install nginx -y > /dev/null
 
-echo "Configuring Nginx"
-cp /var/www/provision/config/nginx_vhost /etc/nginx/sites-available/nginx_vhost > /dev/null
-ln -s /etc/nginx/sites-available/nginx_vhost /etc/nginx/sites-enabled/
-rm -rf /etc/nginx/sites-available/default
-service nginx restart > /dev/null
-
 echo "Preparing MySQL"
 apt-get install debconf-utils -y > /dev/null
 debconf-set-selections <<< "mysql-server mysql-server/root_password password 1234"
@@ -21,3 +15,12 @@ debconf-set-selections <<< "mysql-server mysql-server/root_password_again passwo
 
 echo "Installing MySQL"
 apt-get install mysql-server -y > /dev/null
+
+echo "Configuring Nginx"
+cp /var/www/provision/config/nginx_vhost /etc/nginx/sites-available/nginx_vhost > /dev/null
+ln -s /etc/nginx/sites-available/nginx_vhost /etc/nginx/sites-enabled/
+rm -rf /etc/nginx/sites-available/default
+# Restart Nginx for the config to take effect
+service nginx restart > /dev/null
+
+echo "Finished provisioning."
