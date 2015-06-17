c99419c6c5908322997314979c846162312fab33
Grace Mekarski <gracemekarski@Grace-Mekarski-2.local>
2015-06-17 15:32:25 -0700

    remove git and MySql from provision setup

--- provision/setup.sh
+++ provision/setup.sh
@@ -2,19 +2,8 @@
 
 echo "Provisioning virtual machine..."
 
-echo "Installing Git"
-apt-get install git -y > /dev/null
-
 echo "Installing Nginx"
 apt-get install nginx -y > /dev/null
-
-echo "Preparing MySQL"
-apt-get install debconf-utils -y > /dev/null
-debconf-set-selections <<< "mysql-server mysql-server/root_password password 1234"
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again password 1234"
-
-echo "Installing MySQL"
-apt-get install mysql-server -y > /dev/null
 
 echo "Configuring Nginx"
 cp /var/www/provision/config/nginx_vhost /etc/nginx/sites-available/nginx_vhost > /dev/null
